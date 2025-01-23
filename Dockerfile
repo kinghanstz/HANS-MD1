5485ba9347176acb18e35178c9ee13b12d6f20cf
devhanstz <hanstechmd@gmail.com>
2025-01-24 00:57:37 +0300

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+
+FROM node:lts-buster
+
+RUN apt-get update && \
+  apt-get install -y \
+  ffmpeg \
+  imagemagick \
+  webp && \
+  apt-get upgrade -y && \
+  npm i pm2 -g && \
+  rm -rf /var/lib/apt/lists/*
+  
+RUN git clone https://github.com/devhanstz/HANS-MD-V1 /root/alpha_Bot
+WORKDIR /root/alpha_Bot/
+
+
+COPY package.json .
+RUN npm install pm2 -g
+RUN npm install --legacy-peer-deps
+RUN npm install axios
+
+
+COPY . .
+
+EXPOSE 5000
+
+CMD ["npm", "run" , "hans"]
---
+++ set.js
@@ -0,0 +1,67 @@
+const fs = require('fs-extra');
+const { Sequelize } = require('sequelize');
+if (fs.existsSync('set.env'))
+    require('dotenv').config({ path: __dirname + '/set.env' });
+const path = require("path");
+const databasePath = path.join(__dirname, './database.db');
+const DATABASE_URL = process.env.DATABASE_URL === undefined
+    ? databasePath
+    : process.env.DATABASE_URL;
+module.exports = { session: process.env.SESSION_ID || 'hans',
+    PREFIXE: process.env.PREFIX || "+",
+    GITHUB : process.env.GITHUB|| 'https://github.com/devhanstz/HANS-MD-V1',
+    OWNER_NAME : process.env.OWNER_NAME || "hans",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "255760774888",  
+    ANTI_LINK : process.env.ANTI_LINK || "yes",
+    ANTI_BAD : process.env.ANTI_BAD || "yes",               
+    AUTO_REPLY : process.env.AUTO_REPLY || "yes",              
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "non",
+    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
+    AUTO_REACT: process.env.AUTO_REACTION || "non",  
+    AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || 'non',              
+    CHATBOT: process.env.CHAT_BOT || "off",              
+    AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "yes",
+    AUTO_BLOCK: process.env.BLOCK_ALL || 'yes',              
+    GURL: process.env.GURL  || "https://whatsapp.com/channel/0029VasiOoR3bbUw5aV4qB31",
+    WEBSITE :process.env.GURL || "https://whatsapp.com/channel/0029VasiOoR3bbUw5aV4qB31",
+    CAPTION : process.env.CAPTION || "HANS-MD",
+    BOT : process.env.BOT_NAME || 'HANS-MD',
+    URL : process.env.BOT_MENU_LINKS || '',
+    MODE: process.env.PUBLIC_MODE || "no",              
+    TIMEZONE: process.env.TIMEZONE || "Africa/Tanzania", 
+    PM_PERMIT: process.env.PM_PERMIT || 'no',
+    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME || null,
+    HEROKU_API_KEY : process.env.HEROKU_API_KEY || null,
+    WARN_COUNT : process.env.WARN_COUNT || '3' ,
+    ETAT : process.env.PRESENCE || '',
+    GEMINI_API_KEY : process.env.GEMINI_API_KEY || 'AIzaSyCcZqDMBa8FcAdBxqE1o6YYvzlygmpBx14',
+    DP : process.env.STARTING_BOT_MESSAGE || "yes",
+    ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
+    ANTICALL: process.env.ANTICALL || 'yes',              
+    CHAT_BOT : process.env.CHAT_BOT || 'no',  
+    DATABASE_URL,
+    DATABASE: DATABASE_URL === databasePath
+        ? "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9",
+    /* new Sequelize({
+     dialect: 'sqlite',
+     storage: DATABASE_URL,
+     logging: false,
+})
+: new Sequelize(DATABASE_URL, {
+     dialect: 'postgres',
+     ssl: true,
+     protocol: 'postgres',
+     dialectOptions: {
+         native: true,
+         ssl: { require: true, rejectUnauthorized: false },
+     },
+     logging: false,
+}),*/
+};
+let fichier = require.resolve(__filename);
+fs.watchFile(fichier, () => {
+    fs.unwatchFile(fichier);
+    console.log(`mise à jour ${__filename}`);
+    delete require.cache[fichier];
+    require(fichier);
+});
